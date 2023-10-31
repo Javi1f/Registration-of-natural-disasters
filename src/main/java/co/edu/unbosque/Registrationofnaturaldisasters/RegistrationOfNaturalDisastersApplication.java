23d33eab17ec9f13b9287f11413f29d413f4c2d9
Javi1f <javifemeza76@gmail.com>
2023-10-31 14:49:52 -0500

    Proyect Creation
    The spring proyect has been created to start programming

---
+++ src/main/java/co/edu/unbosque/Registrationofnaturaldisasters/RegistrationOfNaturalDisastersApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.Registrationofnaturaldisasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationOfNaturalDisastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationOfNaturalDisastersApplication.class, args);
+	}
+
+}
